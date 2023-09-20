38d5ca80564aef188be357af387677f02b1fffdc
Spencer Ashcraft <sashcraft121@gmail.com>
2023-09-20 15:11:02 -0600

    javascript finished

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -1,4 +1,33 @@
 /* W02-Task - Profile Home Page */
+let fullName = 'Spencer Ashcraft';
+let foods = ['Cream Puffs', 'Pizza', "S'More Cereal", 'Apples'];
+let foodsAsString = foods.join(', ');
+let currentYear = 2023;
+let profilePic = new Image();
+profilePic.src = 'resume_picture.jpg';
+
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
+
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute('src', 'images/resume_picture.jpg');
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+foodElement.innerHTML = `${foodsAsString}`;
+
+let food = 'Egg Sandwiches';
+foods.push(food);
+foodsAsString = foods.join(', ');
+foodElement.innerHTML += `<br>${foodsAsString}`;
+foods.shift();
+foodsAsString = foods.join(', ');
+foodElement.innerHTML += `<br>${foodsAsString}`;
+foods.pop();
+foodsAsString = foods.join(', ');
+foodElement.innerHTML += `<br>${foodsAsString}`;
+
 
 /* Step 1 - Setup type tasks - no code required */
 
