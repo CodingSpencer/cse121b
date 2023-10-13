a77e38b1da14b84aa8201d298c6d12610d11a891
Spencer Ashcraft <sashcraft121@gmail.com>
2023-10-13 11:58:35 -0600

    fetch with no stretch

---
+++ html/scripts/fetch.js
@@ -0,0 +1,46 @@
+const url = "https://pokeapi.co/api/v2/pokemon/ditto";
+const urlList = "https://pokeapi.co/api/v2/pokemon";
+let results = null;
+
+async function getPokemon(url) {
+  const response = await fetch(url);
+  //check to see if the fetch was successful
+  if (response.ok) {
+    // the API will send us JSON...but we have to convert the response before we can use it
+    // .json() also returns a promise...so we await it as well.
+    const data = await response.json();
+    doStuff(data);
+  }
+}
+function doStuff(data) {
+    const outputElement = document.querySelector("#output");
+    results = data;
+    const html = `<h2>${results.name}</h2>
+                  <img src="${results.sprites.front_default}" alt="Image of ${results.name}">`;
+    outputElement.innerHTML = html;
+    console.log("first: ", results);
+  }
+
+async function getPokemonList(url) {
+    const response = await fetch(url);
+    if (response.ok) {
+        const data = await response.json();
+        doStuffList(data);
+    }
+}
+
+function doStuffList(data) {
+    const listElement = document.querySelector("#outputList");
+    const pokeList = data.results;
+    pokeList.forEach((currentItem) => {
+        const html = `<li>${currentItem.name}</li>`;
+        listElement.innerHTML += html;
+    });
+}
+
+
+
+getPokemon(url);
+console.log("second: ", results);
+
+getPokemonList(urlList);
